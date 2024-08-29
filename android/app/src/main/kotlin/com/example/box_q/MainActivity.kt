30f2d7e865bb9a2eabc620dd06abe254f9ebd00f
Mirza-Mahmud-2021 <mirzamahmud.cse.bubt202@gmail.com>
2024-08-29 13:01:33 +0600

    ready_basic_structure

---
+++ android/app/src/main/kotlin/com/example/box_q/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.box_q
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:box_q/my_app.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(const MyApp());
+}
